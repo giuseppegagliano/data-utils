6cfe16d99cef7859718bc8c6fc5b0a9dffd09d09
Giuseppe Gagliano <gagliano.giuseppe.antonio@gmail.com>
2018-01-13 15:48:32 +0100

    Added functionalities
    Possibility to add label to a noise and WIP on removing label to training
    set.

--- manipulating/add_noise.py
+++ manipulating/add_noise.py
@@ -1,11 +1,15 @@
+# ADD LABELED NOISE POINTS
+
 import pandas as pd
 import numpy as np
 import math
 import os
 
+noise_label = -1
 noise_perc = 5
 noise_range = [-100, 100]
 input = sys.argv[0]
+#input = "pendigits_full_with_head.csv"
 output = input.replace(".csv", "_noise" + str(noise_perc) + ".csv")
 
 # Find input csv shape
@@ -16,6 +20,7 @@ data = pd.concat(txt, ignore_index=True)
 # Add noise points to random position
 noise_count = math.floor(num_rows * noise_perc / 100)
 noise = np.random.uniform(low=noise_range[0], high=noise_range[1], size=(noise_count, num_cols))
+noise[:, num_cols-1] = noise_label
 df = pd.DataFrame(data=noise, columns=data.columns)
 data = data.append(df)
 data = data.sample(frac=1)
--- manipulating/split_train_test.py
+++ manipulating/split_train_test.py
@@ -1,18 +1,23 @@
+# SPLIT DATASET IN TRAINING & TESTING
+
 import pandas as pd
 import numpy as np
 import math
 import os
 
 input = sys.argv[0]
+#input = "pendigits_full_with_head_noise5.csv"
 train_perc = .7
 output_test = input.replace(".csv", "_test.csv")
 output_train = input.replace(".csv", "_train.csv")
 
-# Load & split data 
+# Load & Separate data 
 txt = pd.read_csv(input, sep=',', iterator=True, chunksize=10000)
 data = pd.concat(txt, ignore_index=True)
 train_size = math.floor(data.shape[0] * train_perc)
-test, train = data[:train_size], data[train_size:]
+train, test = data[:train_size], data[train_size:]
+# if labeled
+# train, test = data.iloc[:train_size, :(data.shape[1]-1)], data[train_size:]
 
 # Write to file
 test.to_csv(output_test, sep=',', index=False)
