59cb27356f946920d0af5c9f1e184f66fe14923b
Giuseppe Gagliano <gagliano.giuseppe.antonio@gmail.com>
2018-01-15 23:37:21 +0100

    Add drop option in mongo importer

--- importing/mongo-importer.bat
+++ importing/mongo-importer.bat
@@ -2,6 +2,6 @@ TITLE MongoDB CSV Importer
 SET "MONGO_HOME=C:\Program Files\MongoDB\Server\3.6"
 SET db=datasets
 
-for %%v in (*.csv) do "%MONGO_HOME%\bin\mongoimport.exe" -d %db% -c %%~nv --type CSV --file %%v --headerline
+for %%v in (*.csv) do "%MONGO_HOME%\bin\mongoimport.exe" -d %db% -c %%~nv --type CSV --file %%v --headerline --drop
 TITLE "Import completed!"
 PAUSE
