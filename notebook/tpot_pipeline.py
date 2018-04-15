2c1683d9c2f3fc782d95a242ce5f03da6d600868
Giuseppe Gagliano <gagliano.giuseppe.antonio@gmail.com>
2018-04-15 17:25:24 +0200

    Added classifier comparator

--- notebook/tpot_pipeline.py
+++ notebook/tpot_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.kernel_approximation import RBFSampler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.kernel_approximation import Nystroem
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 
@@ -11,10 +11,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:0.982213438735178
+# Score on the training set was:0.9821937321937323
 exported_pipeline = make_pipeline(
-    RBFSampler(gamma=0.25),
-    GradientBoostingClassifier(learning_rate=0.001, max_depth=4, max_features=0.7000000000000001, min_samples_leaf=9, min_samples_split=9, n_estimators=100, subsample=0.9000000000000001)
+    Nystroem(gamma=0.65, kernel="cosine", n_components=4),
+    RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.7000000000000001, min_samples_leaf=4, min_samples_split=20, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
